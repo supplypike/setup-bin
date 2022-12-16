633a101ecfcd3f016fbdf2c758aecac00a3fdc44
Harry Gogonis <harry@supplypike.com>
2022-12-16 10:15:23 -0500

    upgrade packages and use node16

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ This action sets up a binary in \$PATH for use in actions
 
 ```yaml
 steps:
-  - uses: actions/checkout@v2
-  - uses: supplypike/setup-bin@v1
+  - uses: actions/checkout@v3
+  - uses: supplypike/setup-bin@v3
     with:
       uri: 'https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64'
       name: 'docker-compose'
@@ -18,8 +18,8 @@ You can also run install scripts:
 
 ```yaml
 steps:
-  - uses: actions/checkout@v2
-  - uses: supplypike/setup-bin@v1
+  - uses: actions/checkout@v3
+  - uses: supplypike/setup-bin@v3
     with:
       uri: 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip'
           name: 'aws'
--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,8 +5,7 @@ import {tmpdir} from 'os'
 
 test('dowloads and extracts tool', async () => {
   const config: Config = {
-    uri:
-      'https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64',
+    uri: 'https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64',
     name: 'docker-compose',
     version: '1.27.4'
   }
@@ -18,8 +17,7 @@ test('dowloads and extracts tool', async () => {
 
 test('dowloads and extracts tool to subpath', async () => {
   const config: Config = {
-    uri:
-      'https://github.com/prometheus/prometheus/releases/download/v2.29.2/prometheus-2.29.2.linux-amd64.tar.gz',
+    uri: 'https://github.com/prometheus/prometheus/releases/download/v2.29.2/prometheus-2.29.2.linux-amd64.tar.gz',
     name: 'prometheus',
     version: '2.29.2',
     subPath: 'prometheus-2.29.2.linux-amd64'
